a61778081bae9f850d99a52b3abec6cd83068272
Jazzmin Vangeel <jazzvangeel@gmail.com>
2018-09-25 13:23:13 -0400

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Footer extends React.Component {
+	render() {
+		return (
+			<footer className="footer">
+				<h2 className="footer__credit credit">This is our Footer</h2>
+			</footer>
+		)
+	}
+}
+
+export default Footer;
---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Header extends React.Component {
+	render() {
+		return (
+			<header className="header">
+				<h1 className="header__headline headline">This is our headline</h1>
+			</header>
+		)
+	}
+}
+
+export default Header;
