12ad4d2b5cda3d738a5311838b550686cbb47c93
Jazzmin Vangeel <jazzvangeel@gmail.com>
2018-10-05 12:33:01 -0400

    move component logic

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import './App.css';
-// import logo from './logo.svg';
-import firebase from './firebase';
 import Header from './Header';
 import AccountActions from './AccountActions';
 import PlantList from './PlantList';
@@ -9,32 +7,6 @@ import PlantActions from './PlantActions';
 import Footer from './Footer';
 
 class App extends Component {
-
-  // Storing our Plant info from firebase
-  constructor() {
-    super();
-    this.state = {
-      plants: [],
-      userPlant: ''
-    }
-  }
-
-  // Adding Data to Firebase
-  valueCollection = (event) => {
-    this.setState({userPlant: event.target.value })
-  }
-
-  buttonClick = () => {
-    // Do I need to restate this twice? likely not.
-    const dbRef = firebase.database().ref();
-    dbRef.push(this.state.userPlant);
-    this.setState({userPlant: ""})
-  }
-
-  removePlant = (plantId) => {
-    const dbRef = firebase.database().ref(plantId);
-    dbRef.remove();
-  }
 
   render() {
     return (
@@ -46,44 +18,12 @@ class App extends Component {
           <PlantList />
           <PlantActions />
         </main>
-
-        {this.state.plants.map((plant, i) => {
-          return (
-            <React.Fragment>
-              <li key={i}>
-                {plant.name} - {plant.key}
-              </li>
-
-              <button onClick={() => this.removePlant(plant.key)}>remove this plant</button>
-            </React.Fragment>
-          )
-        })}
-
-        <input type="text" onChange={this.valueCollection} id="plantName" name="plantName" placeholder="Plant Name" />
-        <button onClick={this.buttonClick} className="plant-actions__action">Add Plant</button>
         
         <AccountActions />
 
         <Footer />
       </div>
     )
-  }
-
-  componentDidMount() {
-    const dbRef = firebase.database().ref();
-    dbRef.on('value', (response) => {
-      const newState = [];
-      const data = response.val();
-
-      for (let key in data) {
-        newState.push({key: key, name: data[key]});
-      }
-
-      this.setState({
-        plants: newState
-      });
-      console.log(response.val());
-    });
   }
 }
 
--- src/Plant.js
+++ src/Plant.js
@@ -1,18 +1,21 @@
 import React from 'react';
-import plants from './plants.js';
+import firebase from './firebase';
 
 class Plant extends React.Component {
+
+	// Remove a plant from the dataB
+	removePlant = (plantId) => {
+		const dbRef = firebase.database().ref(plantId);
+		dbRef.remove();
+	}
+
 	render() {
 		return (
-			<React.Fragment>
-				{plants.map((plant) => {
-					return (
-						<div className="plantList__plant plant">
-							<h4 className="plant__headline">{plant.name}</h4>
-						</div>
-					)
-				})}
-			</React.Fragment>
+			<div className="plantList__plant plant">
+				<h4 className="plant__headline">{this.props.plantInfo.name}</h4>
+
+				<button onClick={() => this.removePlant(this.props.plantInfo.key)}>Remove this Plant</button>
+			</div>
 		)
 	}
 }
